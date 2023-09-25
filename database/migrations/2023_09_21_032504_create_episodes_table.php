da6c9a74dd4f2a5c7bad2fbbead272ad5ff7c41e
don <d.maeda@jcd.co.jp>
2023-09-25 14:45:22 +0900

    first commit

---
+++ database/migrations/2023_09_21_032504_create_episodes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number');
+            $table->string('title');
+            $table->string('notes');
+            $table->string('audio');
+            $table->string('image');
+            $table->integer('duration_in_seconds');
+            $table->timestamp('released_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('episodes');
+    }
+};
